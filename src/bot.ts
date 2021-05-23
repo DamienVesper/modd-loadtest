df4d21beaf258dd75c296f8ab0c4015120f6b4a0
Damien Vesper <ldamienvesper@gmail.com>
2021-05-23 12:07:29 -0600

    start connecting the sockets

--- src/bot.ts
+++ src/bot.ts
@@ -8,13 +8,27 @@ import { WebSocket } from '@clusterws/cws';
 const createBot = () => {
     const socket: WebSocket = new WebSocket(`ws://${config.ip}:${config.port}`, `netio1`);
 
-    socket.onopen = () => {
+    socket.on(`open`, () => {
         log(`magenta`, `Bot connected.`);
 
         socket.send(JSON.stringify([`@`, `1`]));
 
-        const int = randomInt(1000, 1999);
-    };
+        const int = randomInt(1000, 2000);
+        socket.send(JSON.stringify([`\u0004`, { number: int, isAdBlockEnabled: false }]));
+        socket.send(JSON.stringify([`\n`, [0, 0]]));
+
+        setInterval(() => {
+            const keys = [`w`, `a`, `s`, `d`];
+            const int = randomInt(0, keys.length - 1);
+
+            for (let i = 0; i < keys.length; i++) socket.send(JSON.stringify([`\t`, { device: `key`, key: keys[i] }]));
+            socket.send(JSON.stringify([`\b`, { device: `key`, key: keys[int] }]));
+        }, 500);
+    });
+
+    socket.on(`close`, () => {
+        log(`red`, `Bot disconnected.`);
+    });
 
     return socket;
 };
