6baf0c28d2fa61b46169b26ddf378e03ad1603c6
Damien Vesper <ldamienvesper@gmail.com>
2021-06-07 15:13:17 -0500

    bot: start movement map (WIP)

--- src/bot.ts
+++ src/bot.ts
@@ -4,6 +4,7 @@ import randomInt from './utils/randomInt';
 import log from './utils/log';
 
 import { WebSocket } from '@clusterws/cws';
+import randomString from './utils/randomString';
 
 const createBot = async (id: number) => {
     const socket: WebSocket = new WebSocket(`${config.ssl ? `wss` : `ws`}://${config.ip}:${config.port}/?token=${config.token}`);
@@ -12,10 +13,20 @@ const createBot = async (id: number) => {
         log(`magenta`, `[${id}] Bot connected.`);
 
         const int = randomInt(1000, 2000);
-        socket.send(JSON.stringify([`@`, `1`]));
+        // const keys = [`w`, `a`, `s`, `d`];
 
-        socket.send(JSON.stringify([`\u0004`, { number: int, isAdBlockEnabled: false }]));
+        socket.send(JSON.stringify([`@`, `1`]));
         socket.send(JSON.stringify([`\n`, [0, 0]]));
+
+        socket.send(JSON.stringify([`\u0004`, {
+            number: int,
+            _id: undefined,
+            sessionId: randomString(32),
+            isAdBlockEnabled: false
+        }]));
+
+        // for (let i = 0; i < keys.length; i++) socket.send(JSON.stringify([`\t`, { device: `key`, key: keys[i] }]));
+        // socket.send(JSON.stringify([`\b`, { device: `key`, key: randomInt(0, keys.length - 1) }]));
     });
 
     socket.on(`error`, (err) => log(`red`, `[${id}] Bot failed to connect to server: ${err.message}.`));
